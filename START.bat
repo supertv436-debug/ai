4a3b96f176cab3885b80053ad700b7a1e74e35d6
supertv436-debug <supertv436@gmail.com>
2026-04-07 22:12:33 +0500

    Add files via upload

---
+++ START.bat
@@ -0,0 +1,3 @@
+@echo off
+.\python-3.10.11.amd64\python.exe main.py
+pause
---
+++ install_lib.bat
@@ -0,0 +1,4 @@
+@echo off
+echo [SYSTEM]: Доустановка модулей ИИ...
+.\python-3.10.11.amd64\python.exe -m pip install requests PyMuPDF python-docx
+pause
